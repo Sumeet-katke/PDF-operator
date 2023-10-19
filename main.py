084d0b0780c246f5acaf8f21b9388ed247be5b7b
Sumeet-katke <snkatke9874@gmail.com>
2023-10-20 00:30:43 +0530

    Updates

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class ILovePDFAPI:
     
     #program to vreate the task
     def create_task(self, task_name, file):
+
+        #making the request url
         url = f'{self.api_url}/{self.tasks[task_name]}'
         headers = {
             'Authorization': f'token {self.api_key}'
@@ -28,25 +30,16 @@ class ILovePDFAPI:
             'task': 'create'
         }
 
-        try:
-            response = requests.post(url, headers=headers, data=data)
+        #getting response
+        response = requests.post(url, headers=headers, data=data)
 
-            if response is not None:
-                if response.status_code == 200:
-                    task_id = response.json().get('task')
-                    if task_id is not None:
-                        return task_id
-                    else:
-                        print(f'Error: Task ID not found in the response.')
-                else:
-                    print(f'Error Creating task: {response.status_code}')
-                    print(f'Error Details: {response.json()}')  # Display the error response for debugging
-            else:
-                print('Error: No response received from the API.')
-
-        except requests.exceptions.RequestException as e:
-            print(f'Error: Failed to connect to the ILOVEPDF API. {e}')
-
+        #If the webpage works it will send code '200'
+        if response.status_code == 200:
+            task_id = response.json()['task']
+            return task_id
+        else:
+            print(f'Error Creating task: {response.status_code}')
+            return None
 
     #Function to upload the PDF files:
     def upload_file(self, task_id, file_path):
